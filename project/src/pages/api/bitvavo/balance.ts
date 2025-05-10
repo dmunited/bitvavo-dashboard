21c1916b061d07a15104c3d801cf5ae3ec19c6d6
Cas TImi <cas@dmunited.nl>
2025-05-11 00:35:05 +0200

    Fix: typo 'kkimport' -> 'import'

--- project/src/pages/api/bitvavo/balance.ts
+++ project/src/pages/api/bitvavo/balance.ts
@@ -1,4 +1,4 @@
-kkimport type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Only allow GET requests
