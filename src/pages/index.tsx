289e438d56013ca85dca769054f4848ec45bbb13
dmunited <143096874+dmunited@users.noreply.github.com>
2025-05-12 10:09:39 +0200

    Updated index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,1 @@
-import dynamic from 'next/dynamic';
-
-// ✅ Gebruik het juiste pad naar components-map
 const Home = dynamic(() => import('../components/index.client'), { ssr: false });
-
-export default Home;
