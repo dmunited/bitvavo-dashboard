8c02782ac4aebc64993cfd86ffbe5c699fa79fa7
dmunited <143096874+dmunited@users.noreply.github.com>
2025-05-12 19:49:22 +0200

    Fix Netlify build error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import dynamic from 'next/dynamic';
 
-// Disable SSR for this client-only component
+// Disable SSR for the Home component
 const Home = dynamic(() => import('../components/index.client'), { ssr: false });
+
+// Force dynamic rendering for Netlify
+export const config = {
+  unstable_runtimeJS: true,
+  unstable_JsPreload: false
+};
 
 export default Home;
