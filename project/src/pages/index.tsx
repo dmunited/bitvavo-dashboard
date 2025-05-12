5f7f15fbe90e0bf8324d3f9163101fb983252000
dmunited <143096874+dmunited@users.noreply.github.com>
2025-05-12 19:46:00 +0200

    Updated index.tsx

--- project/src/pages/index.tsx
+++ project/src/pages/index.tsx
@@ -1,7 +1,6 @@
-// @ts-expect-error
-export const dynamic = 'force-dynamic'; 
+// @ts-expect-error dynamic is a reserved export used by Netlify for SSR
+export const dynamic = 'force-dynamic';
 
-// for Netlify SSR
 import dynamic from 'next/dynamic';
 
 const Home = dynamic(() => import('../components/index.client'), { ssr: false });
