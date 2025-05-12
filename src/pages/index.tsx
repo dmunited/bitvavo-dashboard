fb93f94ce5bf2b87a9b94cf3d8db2bf4865a9d8a
dmunited <143096874+dmunited@users.noreply.github.com>
2025-05-12 11:56:44 +0200

    Fix dynamic import path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic';
 
+// Disable SSR for this client-only component
 const Home = dynamic(() => import('../components/index.client'), { ssr: false });
 
 export default Home;
