9ce209880a8429b128b470b5ab7f80f18968b150
dmunited <143096874+dmunited@users.noreply.github.com>
2025-05-12 16:02:43 +0200

    Updated index.tsx

--- project/src/pages/index.tsx
+++ project/src/pages/index.tsx
@@ -1,6 +1,7 @@
+export const dynamic = 'force-dynamic';
+
 import dynamic from 'next/dynamic';
 
-// Trigger redeploy - v1.3
 // Voorkom SSR van de pagina zodat useSession() niet crasht bij build
 const Home = dynamic(() => import('../components/index.client'), { ssr: false });
 
