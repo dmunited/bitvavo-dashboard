deafed3ed65f58c4e5b830ba87a3c9483bad85cc
dmunited <143096874+dmunited@users.noreply.github.com>
2025-05-12 20:01:54 +0200

    Updated index.tsx

--- project/src/pages/index.tsx
+++ project/src/pages/index.tsx
@@ -1,6 +1,5 @@
-// src/pages/index.tsx
-import { dynamic } from './force-dynamic'; // haal 'force-dynamic' binnen
-import dynamicImport from 'next/dynamic';
+import { dynamic as forceDynamic } from './force-dynamic'; // <-- alias gebruiken om naamconflict te vermijden
+import dynamicImport from 'next/dynamic'; // <-- hernoem deze import om geen conflict te veroorzaken
 
 const Home = dynamicImport(() => import('../components/index.client'), { ssr: false });
 
